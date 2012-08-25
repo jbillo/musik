945c3306e3da8d3644681a02e8af33691b17b726
Jonathan Fritz <jonfritz@gmail.com>
2012-08-25 14:23:28 -0400

    API class is now capable of parsing urls composed of arbitrary /<tag>/<value>/ pairs. Next step is to hook up a database.

--- musik.py
+++ musik.py
@@ -1,40 +1,32 @@
 import cherrypy
+import re
 
-class Albums:
-	def index(self):
-		return "A list of all known albums"
-	index.exposed = True
+# defines an api with a dynamic url scheme composed of /<tag>/<value>/ pairs
+# these pairs are assembled into an SQL query. Each term is combined with the AND operator.
+# unknown <tag> elements are ignored.
+class API:
 
-	def default(self, album):
-		return "Album page for %s" %album
+	def default(self, *params):
+		#TODO: pull all available tags from the database so this regex is dynamic and allows any tag
+		regex = re.compile("genre|artist|album|track")
+
+		#TODO: instead of a string, assemble an SQL query
+		str = ""
+		for index in xrange(0, len(params), 2):
+			if regex.match(params[index]):
+				str += params[index] + ": "
+
+				if len(params) > (index + 1):
+					str += params[index + 1] + "<br />"
+				else:
+					str += "* <br />"
+
+		return str
 	default.exposed = True
-
-
-class Artists:
-	albums = Albums()
-	
-	def index(self):
-		return "A list of all known artists"
-	index.exposed = True
-
-	def default(self, artist):
-		return "Artist page for %s" %artist
-	default.exposed = True
-
-
-# defines the restful api that all client applications use
-class Api:
-	artists = Artists()
-	albums = Albums()
-
-	def index(self):
-		return "Welcome to the API"
-	index.exposed = True
-
 
 # defines the web application that is the default client
 class Musik:
-	api = Api()
+	api = API()
 
 	def index(self):
 		return "Welcome to the Application"
