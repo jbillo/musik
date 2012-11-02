03d11db6245c3295034d6bf2a87871f7fe9648f2
MusikPolice <jonfritz@gmail.com>
2012-11-01 20:26:44 -0400

    re-jigged page rendering functions to include a method that renders header/footer elements as well as one that does not

--- musik/web/application.py
+++ musik/web/application.py
@@ -20,63 +20,58 @@ templates = TemplateLookup(directories=['templates'], module_directory='template
 # A plugin to help SQLAlchemy bind correctly to CherryPy threads
 # See http://www.defuze.org/archives/222-integrating-sqlalchemy-into-a-cherrypy-application.html
 class SAEnginePlugin(plugins.SimplePlugin):
-    def __init__(self, bus):
-        plugins.SimplePlugin.__init__(self, bus)
-        self.sa_engine = None
-        self.bus.subscribe(u'bind', self.bind)
+	def __init__(self, bus):
+		plugins.SimplePlugin.__init__(self, bus)
+		self.sa_engine = None
+		self.bus.subscribe(u'bind', self.bind)
 
-    def start(self):
-    	self.db = DatabaseWrapper()
-        self.sa_engine = self.db.get_engine()
+	def start(self):
+		self.db = DatabaseWrapper()
+		self.sa_engine = self.db.get_engine()
 
-    def stop(self):
-        if self.sa_engine:
-            self.sa_engine.dispose()
-            self.sa_engine = None
+	def stop(self):
+		if self.sa_engine:
+			self.sa_engine.dispose()
+			self.sa_engine = None
 
-    def bind(self, session):
-        session.configure(bind=self.sa_engine)
+	def bind(self, session):
+		session.configure(bind=self.sa_engine)
 
 # A tool to help SQLAlchemy correctly dole out and clean up db connections
 # See http://www.defuze.org/archives/222-integrating-sqlalchemy-into-a-cherrypy-application.html
 class SATool(cherrypy.Tool):
-    def __init__(self):
-        cherrypy.Tool.__init__(self, 'on_start_resource', self.bind_session, priority=20)
-        self.session = scoped_session(sessionmaker(autoflush=True, autocommit=False))
+	def __init__(self):
+		cherrypy.Tool.__init__(self, 'on_start_resource', self.bind_session, priority=20)
+		self.session = scoped_session(sessionmaker(autoflush=True, autocommit=False))
 
-    def _setup(self):
-        cherrypy.Tool._setup(self)
-        cherrypy.request.hooks.attach('on_end_resource', self.commit_transaction, priority=80)
+	def _setup(self):
+		cherrypy.Tool._setup(self)
+		cherrypy.request.hooks.attach('on_end_resource', self.commit_transaction, priority=80)
 
-    def bind_session(self):
-        cherrypy.engine.publish('bind', self.session)
-        cherrypy.request.db = self.session
+	def bind_session(self):
+		cherrypy.engine.publish('bind', self.session)
+		cherrypy.request.db = self.session
 
-    def commit_transaction(self):
-        cherrypy.request.db = None
-        try:
-            self.session.commit()
-        except:
-            self.session.rollback()
-            raise
-        finally:
-            self.session.remove()
+	def commit_transaction(self):
+		cherrypy.request.db = None
+		try:
+			self.session.commit()
+		except:
+			self.session.rollback()
+			raise
+		finally:
+			self.session.remove()
 
 # defines the web application that is the default client
 class Musik:
 	api = api.API()
 
-	def _header(self, **kwargs):
-		return templates.get_template('header.html').render(**kwargs)
 
-	def _footer(self, **kwargs):
-		return templates.get_template('footer.html').render(**kwargs)
-
-	def _render(self, template_name, **kwargs):
-		return templates.get_template(template_name).render(**kwargs)
-
-	# Render an entire page with the passed variables
-	def _render_page(self, template_names, **kwargs):
+	def _render(self, template_names, **kwargs):
+		"""Renders the specified template.
+        template_names arg can be a single template name or a list of templates that
+        will be rendered in order.
+        """
 		result = []
 
 		if type(template_names) != list:
@@ -90,13 +85,30 @@ class Musik:
 			if var not in kwargs:
 				kwargs[var] = u''
 
-		result.append(self._header(**kwargs))
-
 		for template in template_names:
-			result.append(self._render(template, **kwargs))
+			result.append(templates.get_template(template).render(**kwargs))
 
-		result.append(self._footer(**kwargs))
 		return result
+
+
+	def _render_page(self, template_names, **kwargs):
+		"""Renders the specified template as a page complete with header and footer.
+        template_names arg can be a single template name or a list of templates that
+        will be rendered in order.
+        Available kwargs are as follows
+        * title - the title of the page that will appear in the title bar of the browser.
+        * js_appends - a list of javascript files to append in the footer of the page.
+        """
+		if type(template_names) != list:
+			# Create one-element list
+			template_names = [template_names]
+
+		# include the header and footer in the template list
+		template_names.insert(0, 'header.html')
+		template_names.append('footer.html')
+
+		return self._render(template_names, **kwargs)
+
 
 	@cherrypy.expose
 	def index(self):
@@ -104,13 +116,16 @@ class Musik:
 			"title": "Home",
 		})
 
+
 	@cherrypy.expose
 	def albums(self):
 		return self._render("albums.html")
 
+
 	@cherrypy.expose
 	def artists(self):
 		return self._render("artists.html")
+
 
 	@cherrypy.expose
 	def importmedia(self):
@@ -122,19 +137,19 @@ class Musik:
 
 # starts the web app. this call will block until the server goes down
 class MusikWebApplication:
-    log = threads = None
+	log = threads = None
 
-    def __init__(self, log, threads):
-        self.log = log
-        self.threads = threads
+	def __init__(self, log, threads):
+		self.log = log
+		self.threads = threads
 
-        # Subscribe specifically to the 'stop' method passed by cherrypy.
-        # This lets us cleanly stop all threads executed by the application.
-        SAEnginePlugin(cherrypy.engine).subscribe()
-        cherrypy.engine.subscribe("stop", self.stop_threads)
-        cherrypy.tools.db = SATool()
+		# Subscribe specifically to the 'stop' method passed by cherrypy.
+		# This lets us cleanly stop all threads executed by the application.
+		SAEnginePlugin(cherrypy.engine).subscribe()
+		cherrypy.engine.subscribe("stop", self.stop_threads)
+		cherrypy.tools.db = SATool()
 
-        config = {'/':
+		config = {'/':
 			{
 				'tools.db.on': True,
 				'tools.staticdir.root': os.path.dirname(os.path.realpath(sys.argv[0])),
@@ -145,24 +160,24 @@ class MusikWebApplication:
 				'tools.staticdir.dir': "static",
 			},
 		}
-        cherrypy.tree.mount(Musik(), '/', config=config)
+		cherrypy.tree.mount(Musik(), '/', config=config)
 
 
-    # a blocking call that starts the web application listening for requests
-    def start(self, port=8080):
-        cherrypy.config.update({'server.socket_host': '0.0.0.0',})
-        cherrypy.config.update({'server.socket_port': port,})
-        cherrypy.engine.start()
-        cherrypy.engine.block()
+	# a blocking call that starts the web application listening for requests
+	def start(self, port=8080):
+		cherrypy.config.update({'server.socket_host': '0.0.0.0',})
+		cherrypy.config.update({'server.socket_port': port,})
+		cherrypy.engine.start()
+		cherrypy.engine.block()
 
-    # stops the web application
-    def stop(self):
-    	self.log.info(u"Trying to stop main web application")
-        cherrypy.engine.stop()
+	# stops the web application
+	def stop(self):
+		self.log.info(u"Trying to stop main web application")
+		cherrypy.engine.stop()
 
-    # stop all threads before closing out this thread
-    def stop_threads(self):
-        self.log.info(u"Trying to stop all threads")
-        for thread in self.threads:
-            if thread.is_alive():
-                thread.stop()
+	# stop all threads before closing out this thread
+	def stop_threads(self):
+		self.log.info(u"Trying to stop all threads")
+		for thread in self.threads:
+			if thread.is_alive():
+				thread.stop()
