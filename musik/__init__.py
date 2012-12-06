737dde15ed94ff681e7ad7caf77d2d6ba207ec33
Jake Billo <jake@jakebillo.com>
2012-12-05 21:26:11 -0500

    Moved logging directory creation to init function

--- musik/__init__.py
+++ musik/__init__.py
@@ -1,8 +1,17 @@
 import logging
+import os
 
 
 def initLogging(moduleName):
 	# set up logging
+	log_dir = os.path.join(os.path.dirname(__file__), "logs")  # default logging location
+
+	if not os.path.isdir(log_dir):
+		try:
+			os.mkdir(log_dir)
+		except IOError:
+			print u"Could not create log directory {0}; this will likely cause problems".format(log_dir)
+
 	log = logging.getLogger(moduleName)
 	log.setLevel(logging.DEBUG)
 
@@ -10,7 +19,7 @@ def initLogging(moduleName):
 	ch = logging.StreamHandler()
 	ch.setLevel(logging.DEBUG)
 
-	fs = logging.FileHandler(u'logs/%s.log' % moduleName)
+	fs = logging.FileHandler(u'{0}/{1}.log'.format(log_dir, moduleName))
 	fs.setLevel(logging.DEBUG)
 
 	# create formatter
